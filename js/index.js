5a280ff8d8c0bbb49b05fdd9c141b0853d3cb556
melaniehall <melaniehalldesign@gmail.com>
2013-02-28 10:07:04 -0600

    Added Team Info Toggle

--- js/index.js
+++ js/index.js
@@ -1,24 +1,34 @@
 $(document).ready(function(){
   // get from database
-  //on load, populate table listTEams
+  //on load, populate table listTeams
+  leagueData=[];
+
   listTeams = function(){
     $.ajax({
       url: '/backliftapp/teams',
       type: "GET",
       dataType: 'json',
       success: function(data) {
+        leagueData = data;
         //Add to teams list table
-        var table = $('#standings')
+        var table = $('#standingsTable')
         table.html("")
         //loop (team)
           for(var i=0; i< data.length; i++){
-        table.append('<tr><td>' + data[i].name + '</td><td>' + data[i].wins + '</td><td>' + data[i].losses + '</td><td>' + (data[i].wins)/(data[i].totalGames) + '</td></tr>')};
-      },
+        table.append('<tr><td><a>' + data[i].name + '</a><p class="moreinfo">Manager: '+ data[i].mgrFirst + ' '+ data[i].mgrLast + '  |  Phone: '+ data[i].phone + '<br/> Sponsored by: ' + data[i].sponsor + '</p></td><td>' + data[i].wins + '</td><td>' + data[i].losses + '</td><td>' + (data[i].wins)/(data[i].totalGames) + '</td></tr>')};
+        $(".moreinfo").hide()
+        $("td a").click(function(){
+          $(this).next(".moreinfo").toggle();
+        });
+      },      
+
       error: function(data){
         alert("errors")
       }
     })
   }
+
+
 
   
   saveTeam = function(team){
@@ -28,6 +38,8 @@ $(document).ready(function(){
       dataType: 'json',
       data: team,
       success: function(data) {
+        leagueData.push(data);
+        
         //Add to teams list table
         listTeams()
       }, 
@@ -101,66 +113,14 @@ $(document).ready(function(){
     listTeams();
 
 
+// *************Clear Form
+
     function clearForm(){
       $('.team_inputs').each(function(){
         $(this).val('');
       });//end each
     };//end clearForm
 
-    // $("#addteam").click(function(){
-    //   var firstName= $("#inputFirst").val();
-    //   $("table").append("<tr><td>" + firstName + "</td></tr>");
-    //   $("#myModal").modal('hide');
-    //   console.log("hello")
-    // });
-
-// Trying Serialize
-// var teamList= [];
-// $("#addteam").click(function(){
-//   var team= $(":input").serializeArray();
-
-//   jQuery.each(team, function(key, value){
-//     alert(key.value + ": " + value.value);
-//     // $("table").append("<tr><td>"+obj.value.value+"</td></tr>");
-//   })
-// $("#myModal").modal('hide');
-
-
-
-
-
-// var Team= function(teamName, mgrFirst, mgrLast, phone, zip, sponsor){
-//   this.teamName= teamName;
-//   this.mgrFirst= mgrFirst;
-//   this.mgrLast= mgrLast;
-//   this.phone= phone;
-//   this.zip= zip;
-//   this.sponsor= sponsor;
-
-// };
-
-
-
-// $("#addteam").click(function(){
-// var participant= new Team ($("#inputName").val(), $("#inputFirst").val(), $("#inputLast").val(), $("#phone").val(), $("#zip").val(), $("#sponsor").val());
-//   console.log(participant);
-
-//   $("#myModal").modal('hide');
-   
-//   teamList.push(participant);
-
-//   $("table").empty();
-  
-//   jQuery.each(teamList, function(index, value){
-//     $("table").append("<tr><td>" + value.teamName + "</td><td> 0 </td> <td> 0 </td> <td>.000</td> </tr>" );
-//     $("table").append("<p>" + value.mgrFirst + " " + value.mgrLast + "<br/>" + value.phone +"<br/>" + value.sponsor + "</p>");
-//   }); //end jQuery.each  
-
-//   console.log(teamList);
-
-//   $("#form")[0].reset(); 
-
-//  }); //end click
 
 
 
